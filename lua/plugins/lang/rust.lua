5ecf9ccc697ef513ca441ee01ac1f7c2f3036bf1
Freddie Haddad <freddie.haddad@outlook.com>
2024-04-17 16:46:25 -0700

    rust: switch to rustaceanvim

--- lua/plugins/lang/rust.lua
+++ lua/plugins/lang/rust.lua
@@ -2,7 +2,6 @@
 Rust language support:
 
 Install via the rustup tool (https://rustup.rs/):
-
      - curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 LSP
@@ -13,28 +12,36 @@ DAP
 
 Formatter
     - rustfmt (rustup component add rustfmt)
-
-Unit Testing
-    - curl -LsSf https://get.nexte.st/latest/linux | tar zxf - -C ${CARGO_HOME:-~/.cargo}/bin
 --]]
+
 return {
 	-- lsp
 	{
-		'neovim/nvim-lspconfig',
+		'mrcjkb/rustaceanvim',
+		config = function(_, opts) vim.g.rustaceanvim = vim.tbl_deep_extend('keep', vim.g.rustaceanvim or {}, opts or {}) end,
 		dependencies = {
 			'williamboman/mason-lspconfig.nvim',
 			opts = function(_, opts) table.insert(opts.ensure_installed, 'rust_analyzer') end,
 		},
-		opts = function(_, opts) opts.servers.rust_analyzer = {} end,
+		ft = { 'rust' },
+		opts = {
+			server = {
+				on_attach = function(_, bufnr)
+					vim.keymap.set('n', '<leader>ca', function() vim.cmd.RustLsp('codeAction') end, { desc = 'Code action', buffer = bufnr })
+					vim.keymap.set('n', '<leader>dr', function() vim.cmd.RustLsp('debuggables') end, { desc = 'Rust debuggables', buffer = bufnr })
+				end,
+			},
+			tools = {
+				float_win_config = {
+					border = 'none',
+				},
+			},
+		},
 	},
 
 	-- formatting
 	{
 		'stevearc/conform.nvim',
-		dependencies = {
-			'williamboman/mason.nvim',
-			opts = function(_, opts) vim.list_extend(opts.ensure_installed, { 'goimports', 'gofumpt' }) end,
-		},
 		opts = {
 			formatters_by_ft = {
 				rust = { 'rustfmt' },
@@ -49,48 +56,15 @@ return {
 			'williamboman/mason.nvim',
 			opts = function(_, opts) table.insert(opts.ensure_installed, 'codelldb') end,
 		},
-		opts = function()
-			local dap = require('dap')
-			dap.adapters.codelldb = {
-				type = 'server',
-				port = '${port}',
-				executable = {
-					command = 'codelldb',
-					args = { '--port', '${port}' },
-				},
-			}
-			dap.configurations.rust = {
-				{
-					name = 'Launch file',
-					type = 'codelldb',
-					request = 'launch',
-					program = function() return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file') end,
-					cwd = '${workspaceFolder}',
-					stopOnEntry = false,
-					args = {},
-				},
-				{
-					name = 'Attach to process',
-					type = 'codelldb',
-					request = 'attach',
-					pid = require('dap.utils').pick_process,
-					cwd = '${workspaceFolder}',
-				},
-			}
-		end,
 	},
 
 	-- test
 	{
-		'rouge8/neotest-rust',
-		dependencies = {
-			{
-				'nvim-neotest/neotest',
-				opts = {
-					adapters = {
-						['neotest-rust'] = {},
-					},
-				},
+		'nvim-neotest/neotest',
+		dependencies = 'mrcjkb/rustaceanvim',
+		opts = {
+			adapters = {
+				['rustaceanvim.neotest'] = {},
 			},
 		},
 	},
