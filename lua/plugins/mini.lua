21b1f34b0069b52329d7418b3fe7ff7503c51073
Freddie Haddad <freddie.haddad@outlook.com>
2024-04-19 14:20:04 -0700

    feat: add mini.surround

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -27,12 +27,29 @@ return {
 		opts = {},
 	},
 
+	-- surround
+	{
+		'echasnovski/mini.surround',
+		opts = {
+			mappings = {
+				add = 'gsa', -- Add surrounding in Normal and Visual modes
+				delete = 'gsd', -- Delete surrounding
+				find = 'gsf', -- Find surrounding (to the right)
+				find_left = 'gsF', -- Find surrounding (to the left)
+				highlight = 'gsh', -- Highlight surrounding
+				replace = 'gsr', -- Replace surrounding
+				update_n_lines = 'gsn', -- Update `n_lines`
+			},
+		},
+	},
+
 	-- which key integration
 	{
 		'folke/which-key.nvim',
 		opts = {
 			defaults = {
 				['<leader>b'] = { name = '+buffer' },
+				['gs'] = { name = '+surround' },
 			},
 		},
 	},
