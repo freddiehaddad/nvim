b621c61d357ca3ad47324c49f6ce83e9460c72a7
Freddie Haddad <freddie.haddad@outlook.com>
2024-06-12 03:07:01 -0700

    fix(treesitter): revert config changes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -25,8 +25,4 @@ return {
 			enable = true,
 		},
 	},
-	config = function(_, opts)
-		require('nvim-treesitter.install').compilers = { 'cl' }
-		require('nvim-treesitter.configs').setup(opts)
-	end,
 }
