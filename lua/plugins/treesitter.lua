b379bf37eca771efebb4b0c70431ddf6b396aadc
Freddie Haddad <freddie.haddad@outlook.com>
2024-06-12 02:33:50 -0700

    fix(treesitter): set compiler to cl

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -25,4 +25,8 @@ return {
 			enable = true,
 		},
 	},
+	config = function(_, opts)
+		require('nvim-treesitter.install').compilers = { 'cl' }
+		require('nvim-treesitter.configs').setup(opts)
+	end,
 }
