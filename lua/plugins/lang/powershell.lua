ef38ae2716e83df9081960bdb1d140e6176874c4
Freddie Haddad <freddie.haddad@outlook.com>
2024-07-02 09:19:53 -0700

    feat: add powershell support

--- lua/plugins/lang/powershell.lua
+++ lua/plugins/lang/powershell.lua
@@ -5,17 +5,20 @@ LSP
 	- powershell_es
 --]]
 return {
-	-- lsp
-	{
-		'neovim/nvim-lspconfig',
-		dependencies = {
-			'williamboman/mason-lspconfig.nvim',
-			opts = function(_, opts) table.insert(opts.ensure_installed, 'powershell_es') end,
+	'TheLeoP/powershell.nvim',
+	dependencies = {
+		{
+			'neovim/nvim-lspconfig',
+			dependencies = {
+				'williamboman/mason-lspconfig.nvim',
+				opts = function(_, opts) table.insert(opts.ensure_installed, 'powershell_es') end,
+			},
 		},
-		opts = function(_, opts)
-			opts.servers.powershell_es = {
-				bundle_path = vim.fn.stdpath('data') .. '/mason/packages/powershell-editor-services',
-			}
-		end,
+		{
+			'mfussenegger/nvim-dap',
+		},
+	},
+	opts = {
+		bundle_path = vim.fn.stdpath('data') .. '/mason/packages/powershell-editor-services',
 	},
 }
