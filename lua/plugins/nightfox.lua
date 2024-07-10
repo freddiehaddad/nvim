02afef41c57baf5e5ec2d9d0a729cf12f915bf8d
Freddie Haddad <freddie.haddad@outlook.com>
2024-07-09 22:21:52 -0700

    nightfox: set all styles to NONE

--- lua/plugins/nightfox.lua
+++ lua/plugins/nightfox.lua
@@ -5,17 +5,12 @@ return {
 		options = {
 			transparent = true,
 			styles = {
-				-- foot.ini:
-				-- font=IosevkaTerm Nerd Font Mono:weight=regular:size=12
-				-- font-bold=IosevkaTerm Nerd Font Mono:weight=medium:size=12
-				-- font-italic=IosevkaTerm Nerd Font Mono:weight=bold:size=12
-				-- font-bold-italic=IosevkaTerm Nerd Font Mono:weight=black:size=12
 				comments = 'NONE',
 				conditionals = 'NONE',
-				constants = 'bold',
+				constants = 'NONE',
 				functions = 'NONE',
-				keywords = 'bold',
-				numbers = 'bold',
+				keywords = 'NONE',
+				numbers = 'NONE',
 				operators = 'NONE',
 				strings = 'NONE',
 				types = 'NONE',
