5d59372e19e7d2fda8dcd655fa4dbba1285c0d71
Freddie Haddad <freddie.haddad@outlook.com>
2024-10-20 19:49:34 -0700

    fix(nightfox): use underline since windows doesn't support undercurl

--- lua/plugins/nightfox.lua
+++ lua/plugins/nightfox.lua
@@ -31,8 +31,28 @@ return {
         },
         groups = {
             nightfox = {
+                DiagnosticUnderlineError = {
+                    fg = 'diag.error',
+                    style = 'underline',
+                },
+                DiagnosticUnderlineWarn = {
+                    fg = 'diag.warn',
+                    style = 'underline',
+                },
+                DiagnosticUnderlineInfo = {
+                    fg = 'diag.info',
+                    style = 'underline',
+                },
+                DiagnosticUnderlineHint = {
+                    fg = 'diag.hint',
+                    style = 'underline',
+                },
                 StatusLine = { bg = 'none' },
                 StatusLineNC = { bg = 'none' },
+                SpellBad = { fg = 'diag.error', style = 'underline' },
+                SpellCap = { fg = 'diag.warn', style = 'underline' },
+                SpellLocal = { fg = 'diag.info', style = 'underline' },
+                SpellRare = { fg = 'diag.info', style = 'underline' },
             },
         },
     },
