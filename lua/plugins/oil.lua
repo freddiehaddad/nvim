7f7495d14b8f5004765c8552064d7c0c6cf1f153
Freddie Haddad <freddie.haddad@outlook.com>
2024-12-11 19:03:24 -0800

    set oil to lazy load

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,7 @@
 return {
     'stevearc/oil.nvim',
     dependencies = 'nvim-tree/nvim-web-devicons',
+    lazy = true,
     opts = {
         keymaps = {
             ['<c-v>'] = 'actions.select_vsplit',
