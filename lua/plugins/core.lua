fa16fc141b475ff374d0f6c7b28c1d49c5dbc4a3
Freddie Haddad <freddie.haddad@outlook.com>
2024-12-20 15:06:30 -0800

    more keybind fixes

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -291,52 +291,32 @@ return {
                 topdelete = { text = "" },
                 changedelete = { text = "▎" },
             },
-            on_attach = function(buffer)
+            on_attach = function(bufnr)
                 local gs = package.loaded.gitsigns
 
                 local function map(mode, l, r, desc)
-                    vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+                    vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
                 end
 
-                map("n", "]h", function()
-                    if vim.wo.diff then
-                        vim.cmd.normal({ "]c", bang = true })
-                    else
-                        gs.nav_hunk("next")
-                    end
-                end, "Next Hunk")
-                map("n", "[h", function()
-                    if vim.wo.diff then
-                        vim.cmd.normal({ "[c", bang = true })
-                    else
-                        gs.nav_hunk("prev")
-                    end
-                end, "Prev Hunk")
-                map("n", "]H", function()
-                    gs.nav_hunk("last")
-                end, "Last Hunk")
-                map("n", "[H", function()
-                    gs.nav_hunk("first")
-                end, "First Hunk")
-                map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-                map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-                map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
-                map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
-                map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
-                map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
-                map("n", "<leader>ghb", function()
-                    gs.blame_line({ full = true })
-                end, "Blame Line")
-                map("n", "<leader>ghB", function()
-                    gs.blame()
-                end, "Blame Buffer")
-                map("n", "<leader>ghd", gs.diffthis, "Diff This")
-                map("n", "<leader>ghD", function()
-                    gs.diffthis("~")
-                end, "Diff This ~")
-                map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+                -- stylua: ignore start
+                map("n", "]h", function() if vim.wo.diff then vim.cmd.normal({ "]c", bang = true }) else gs.nav_hunk("next") end end, "Next hunk")
+                map("n", "[h", function() if vim.wo.diff then vim.cmd.normal({ "[c", bang = true }) else gs.nav_hunk("prev") end end, "Prev hunk")
+                map("n", "]H", function() gs.nav_hunk("last") end, "Last hunk")
+                map("n", "[H", function() gs.nav_hunk("first") end, "First hunk")
+                map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage hunk")
+                map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset hunk")
+                map("n", "<leader>ghS", gs.stage_buffer, "Stage buffer")
+                map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo stage hunk")
+                map("n", "<leader>ghR", gs.reset_buffer, "Reset buffer")
+                map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview hunk inline")
+                map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame line")
+                map("n", "<leader>ghB", function() gs.blame() end, "Blame buffer")
+                map("n", "<leader>ghd", gs.diffthis, "Diff this")
+                map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff this ~")
+                map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns select hunk")
                 map("n", "<leader>gc", "<cmd>Telescope git_commits<cr>", "Commits")
                 map("n", "<leader>gs", "<cmd>Telescope git_status<cr>", "Status")
+                -- stylua: ignore end
             end,
         },
     },
