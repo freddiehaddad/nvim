498b2cfaf44432627774a366e2eaca0bc58486cb
Freddie Haddad <freddie.haddad@outlook.com>
2024-12-20 09:04:35 -0800

    manually add keys section to fix lazy

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -164,6 +164,18 @@ return {
                 width = 47,
                 preset = {
                     header = header,
+                    keys = {
+                        -- stylua: ignore start
+                        { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+                        { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                        { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+                        { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+                        { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+                        { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+                        { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+                        { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+                        -- stylua: ignore end
+                    },
                 },
                 sections = {
                     { section = "header" },
