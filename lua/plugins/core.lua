01c2fa42064da9621be31a0c9e06a7cae0c5f561
Freddie Haddad <freddie.haddad@outlook.com>
2025-03-14 13:57:45 -0700

    Use the new lsp status feature of lualine

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -143,7 +143,7 @@ return {
                             padding = { left = 1 },
                         },
                     },
-                    lualine_x = {},
+                    lualine_x = { { "lsp_status", icon = "", symbols = { done = "●" }, color = "StatusLine" } },
                     lualine_y = {
                         {
                             "macro",
@@ -215,20 +215,20 @@ return {
                 end,
             })
 
-            vim.api.nvim_create_autocmd("LspProgress", {
-                ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
-                callback = function(ev)
-                    local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
-                    vim.notify(vim.lsp.status(), "info", {
-                        id = "lsp_progress",
-                        title = "LSP Progress",
-                        opts = function(notif)
-                            notif.icon = ev.data.params.value.kind == "end" and " "
-                                or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
-                        end,
-                    })
-                end,
-            })
+            --     vim.api.nvim_create_autocmd("LspProgress", {
+            --         ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+            --         callback = function(ev)
+            --             local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+            --             vim.notify(vim.lsp.status(), "info", {
+            --                 id = "lsp_progress",
+            --                 title = "LSP Progress",
+            --                 opts = function(notif)
+            --                     notif.icon = ev.data.params.value.kind == "end" and " "
+            --                         or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+            --                 end,
+            --             })
+            --         end,
+            --     })
         end,
         opts = {
             dashboard = {
