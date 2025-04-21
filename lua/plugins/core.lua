20a1d40f45e12a10cd48f03dbfd8b84c015905f1
Freddie Haddad <freddie.haddad@outlook.com>
2025-04-20 19:42:20 -0700

    fix: remove unneeded adapter configuration code

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1455,32 +1455,11 @@ return {
             if opts.adapters then
                 local adapters = {}
                 for name, config in pairs(opts.adapters or {}) do
-                    if type(name) == "number" then
-                        if type(config) == "string" then
-                            config = require(config)
-                        end
-                        adapters[#adapters + 1] = config
-                    elseif config ~= false then
-                        local adapter = require(name)
-                        if type(config) == "table" and not vim.tbl_isempty(config) then
-                            local meta = getmetatable(adapter)
-                            if adapter.setup then
-                                adapter.setup(config)
-                            elseif adapter.adapter then
-                                adapter.adapter(config)
-                                adapter = adapter.adapter
-                            elseif meta and meta.__call then
-                                adapter = adapter(config)
-                            else
-                                error("Adapter " .. name .. " does not support setup")
-                            end
-                        end
-                        adapters[#adapters + 1] = adapter
-                    end
+                    local adapter = require(name)
+                    adapters[#adapters + 1] = adapter
                 end
                 opts.adapters = adapters
             end
-
             require("neotest").setup(opts)
         end,
         -- stylua: ignore
