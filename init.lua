faa5cd5185972235ac56ddb4d2512d2691895a29
Freddie Haddad <freddie.haddad@outlook.com>
2025-04-29 15:03:20 -0700

    fix: set maplocalleader to same as mapleader

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ vim.cmd("colorscheme peanut")
 
 -- Options
 vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.g.maplocalleader = " "
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
