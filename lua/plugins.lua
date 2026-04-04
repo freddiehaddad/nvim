51d81706bc96aab2c8f625d4232888f37bbbaf75
Freddie Haddad <freddie.haddad@outlook.com>
2026-04-04 15:02:48 -0700

    feat: highlight line number in sign column for diagnostics

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -142,10 +142,16 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.diagnostic.config({
     signs = {
         text = {
-            [vim.diagnostic.severity.ERROR] = "󰬌 ",
-            [vim.diagnostic.severity.WARN] = "󰬞 ",
-            [vim.diagnostic.severity.INFO] = "󰬐 ",
-            [vim.diagnostic.severity.HINT] = "󰬏 ",
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.INFO] = "",
+            [vim.diagnostic.severity.HINT] = "",
+        },
+        numhl = {
+            [vim.diagnostic.severity.ERROR] = "DiagnosticError",
+            [vim.diagnostic.severity.WARN] = "DiagnosticWarn",
+            [vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+            [vim.diagnostic.severity.HINT] = "DiagnosticWarn",
         },
     },
 })
